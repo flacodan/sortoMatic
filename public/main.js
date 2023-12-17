015144d283ff7658541cf25a7fbeab378c50a54a
Dan Hill <danshill@gmail.com>
2023-12-17 11:17:02 -0700

    can add and prioritize now

---
+++ public/main.js
@@ -0,0 +1,65 @@
+const fullList = document.getElementById('full-list');
+const taskInput = document.getElementById('add-task-field');
+const addButton = document.getElementById('add-button');
+const sortButton = document.getElementById('sort-button');
+const sortedList = document.getElementById('sorted-list');
+const deleteButton = document.getElementById('delete-task-button');
+
+addButton.addEventListener('click', (event) => {
+    if (taskInput.value.length < 1) {
+        alert("I see what you're doing - but we can't do the work for you. Do better.")
+        return;
+    }
+    let maBod = { task: taskInput.value };
+    axios.post('/create-task', maBod)
+    .then((response) => { buildFullTaskList(response.data); })
+    .catch((err) => { console.log(err); })
+})
+
+sortButton.addEventListener('click', (event) => {
+    if (fullList.innerHTML) {
+        axios.get('/sort-task')
+        .then((response) => { buildSortedTaskList(response.data); })
+        .catch((err) => { console.log(err); })
+    }
+})
+
+// deleteForm.addEventListener('submit', (evt) => {
+//     evt.preventDefault();
+//     axios.delete('/delete-fighter/' + deleteInput.value)
+//     .then((result) => {
+//         fillFullTaskList(result.data);})
+//     .catch((err) => {console.log(err);})
+// })
+
+// axios.get('/fighters')
+// .then((result) => {
+//     fillFullTaskList(result.data);
+// })
+// .catch((err) => {
+//     console.log(err);
+// })
+
+function buildFullTaskList(fullTaskArray) {
+    fullList.innerHTML='';
+
+    for(let i=0; i < fullTaskArray.length; i++) {
+        let thisItem = document.createElement('p');
+        thisItem.innerHTML = fullTaskArray[i].task;
+        fullList.appendChild(thisItem);
+    }
+
+}
+
+function buildSortedTaskList(sortedTaskArray) {
+    sortedList.innerHTML='';
+    for(let i=0; i < sortedTaskArray.length; i++) {
+        let thisItem = document.createElement('option');
+        // thisItem.style.border = "none";
+        // thisItem.style.background = "none";
+        thisItem.innerHTML = sortedTaskArray[i].task;
+        sortedList.appendChild(thisItem);
+        console.log(thisItem);
+    }
+
+}
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 let app = express();
+app.use(express.json());
+
+let db = [];
+let sortedDb = [];
+let tasksServed = 0;
 
 app.get('/', (req, res) => {
     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/index.html'))
@@ -14,9 +19,30 @@ app.get('/js', (req, res) => {
     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/main.js'))
 })
 
+app.post('/create-task', (req, res) => {
+    db.push(req.body);
+    tasksServed++;
+    res.send(db);
+})
 
-
+app.get('/sort-task', (req, res) => {
+    res.send(shuffleSort());
+})
 
 app.listen(8000, () => {
     console.log('Server listening on port 8000');
 })
+
+
+
+function shuffleSort() {
+    const uniqueSet = new Set()
+    const max = db.length - 1;
+    sortedDb.length = 0;
+
+    while(uniqueSet.size < 3) {
+      uniqueSet.add(Math.floor(Math.random() * max) + 1);
+    }
+    uniqueSet.forEach((newIndex) => { sortedDb.push(db[newIndex]); })
+    return sortedDb;
+}
